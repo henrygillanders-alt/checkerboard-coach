41ae68f88a54c1f3864f6663e6ef08cc79a0d07b
henrygillanders-alt <henrygillanders@gmail.com>
2026-05-08 10:08:39 +0100

    Create games.js

---
+++ data/games.js
@@ -0,0 +1,33 @@
+ export const sessionGroups = [
+
+  {
+
+    title: "Checkerboard Basics",
+
+    sessions: ["Singles", "Pairs", "Triples"]
+
+  }
+
+];
+
+export const quickStarts = [
+
+  "Win rally +1",
+
+  "Complete pair +2",
+
+  "Win after challenge +3"
+
+];
+
+export const challengeDeck = [
+
+  "[6-4]",
+
+  "[8-1]",
+
+  "[5-3]",
+
+  "[7-2]"
+
+];
